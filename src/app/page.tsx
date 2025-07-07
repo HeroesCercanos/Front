2003b121b5f238c95523ee31372c96961285c3c0
VMGaray <132283775+VMGaray@users.noreply.github.com>
2025-07-06 21:48:12 -0300

    feat: seccion Cuartel con mapa incrustado finalizada

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,9 +1,11 @@
 import Intro from "@/components/landing/Intro";
+import MapaLanding from "@/components/landing/MapaLanding";
 
 export default function Home() {
   return (
     <div>
       <Intro />
+      <MapaLanding />
     </div>
   );
 }
