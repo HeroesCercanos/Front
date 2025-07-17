07e29a045bbb494a7499627fae1d845732dfcd09
Enzo Ibarra <enz0ibarra.cai@gmail.com>
2025-07-17 13:48:10 -0300

    deploy

--- src/app/trainings/[publicId]/page.tsx
+++ src/app/trainings/[publicId]/page.tsx
@@ -2,9 +2,11 @@ import { fetchSingleMedia } from '@/lib/cloudinaryClient';
 import MediaPreview from '@/components/trainings/MediaPreview';
 import { notFound } from 'next/navigation';
 
-type Props = { params: { publicId: string } };
-
-export default async function TrainingDetailPage({ params }: Props) {
+export default async function TrainingDetailPage({
+	params,
+}: {
+	params: { publicId: string };
+}) {
 	const decodedId = decodeURIComponent(params.publicId);
 	const media = await fetchSingleMedia(decodedId);
 	if (!media) return notFound();
