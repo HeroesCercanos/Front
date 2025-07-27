c412f811e88c6f86a0b6fbe049bdf1a7016dbb91
Tano <mario.fuceneco97@gmail.com>
2025-07-27 18:31:21 -0300

    Reset Password

---
+++ src/app/reset-password/page.tsx
@@ -0,0 +1,13 @@
+import ResetPasswordPage from "@/components/common/Reset-password"
+import React from 'react'
+const resetPassword = () => {
+
+    return (
+
+        <div>
+            <ResetPasswordPage />
+        </div>
+
+    );
+};
+export default resetPassword;
