f3cee8a968d566892ffa9e39b0a335fcb308afd2
Enzo Ibarra <enz0ibarra.cai@gmail.com>
2025-08-03 13:39:28 -0300

    dashboard de usuario

--- src/components/dashboard/DashboardCard.tsx
+++ src/components/dashboard/DashboardCard.tsx
@@ -9,20 +9,25 @@ export default function DashboardCard({
 	customButton,
 }: ICardProps) {
 	return (
-		<div className='z-0 bg-white p-6 rounded-2xl shadow-inner shadow-gray-200 drop-shadow-lg flex flex-col items-center text-center space-y-4'>
-			<div>{icon}</div>
-			<h3 className='text-xl font-semibold'>{title}</h3>
-			<p className='text-gray-600'>{description}</p>
-			{customButton ? (
-				customButton
-			) : (
-				<button
-					onClick={onClick}
-					className='mt-4 bg-red-600 hover:bg-red-700 text-white font-semibold py-2 px-4 rounded-lg cursor-pointer'
-				>
-					{buttonLabel}
-				</button>
-			)}
+		<div className='z-0 bg-white p-6 rounded-2xl shadow-inner shadow-gray-200 drop-shadow-lg flex flex-col justify-between text-center items-center h-full min-h-[340px]'>
+			<div className='flex flex-col items-center space-y-4'>
+				<div>{icon}</div>
+				<h3 className='text-xl font-semibold'>{title}</h3>
+				<p className='text-gray-600'>{description}</p>
+			</div>
+
+			<div className='mt-6 w-full flex justify-center'>
+				{customButton ? (
+					customButton
+				) : (
+					<button
+						onClick={onClick}
+						className='bg-red-600 hover:bg-red-700 text-white font-semibold py-2 px-4 rounded-lg cursor-pointer w-auto'
+					>
+						{buttonLabel}
+					</button>
+				)}
+			</div>
 		</div>
 	);
 }
